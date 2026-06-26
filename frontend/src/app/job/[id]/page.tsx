efcd9c70704a26f346fd00e2e8fb979df949f924
Khuzaima Hassan <145604191+KhuzaimaHassan@users.noreply.github.com>
2026-06-26 18:20:44 +0500

    hotfix: fix missing closing div tag in job details page

--- frontend/src/app/job/[id]/page.tsx
+++ frontend/src/app/job/[id]/page.tsx
@@ -172,7 +172,6 @@ export default function JobDetailPage() {
                   />
                 </div>
               </div>
-            </div>
 
             <AICopilotDrawer
               job={job}
